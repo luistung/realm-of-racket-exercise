630f444f5fab4bce92761e527d31fa47eb0b82cd
luistung <dongliang1986@gmail.com>
2025-06-09 21:17:11 +0800

    ADD mcts.rkt

--- chapter12/difficult-1/mcts.rkt
+++ chapter12/difficult-1/mcts.rkt
@@ -3,12 +3,14 @@
 (module mcts-generics racket
   (require racket/generic)
   (provide gen:mcts-state
+           player
            possible-action
            take-action
            terminal?
            process-reward
            policy-score)
   (define-generics mcts-state
+                   (player mcts-state)
                    (possible-action mcts-state)
                    (take-action mcts-state action)
                    (terminal? mcts-state)
@@ -22,45 +24,57 @@
 
   (require (submod ".." mcts-generics))
 
-  (define MAZE-STATE-WIDTH 10)
-  (define MAZE-STATE-HEIGHT 10)
+  (define MAZE-STATE-WIDTH (make-parameter #f))
+  (define MAZE-STATE-HEIGHT (make-parameter #f))
   (define policy-prob (make-parameter #f))
   (define local-reward (make-parameter #f))
 
-  (struct maze-state (x y)
+  (struct maze-state (x y player)
     #:transparent
     #:methods gen:mcts-state
-    [(define (possible-action state)
+    [(define (player mcts-state)
+       (maze-state-player mcts-state))
+     
+     (define (possible-action state)
        (define-values (x y) (values (maze-state-x state) (maze-state-y state)))
        (for/list ([action '(left right up down)]
                   [valid? (list (> x 0)
-                                (< x (sub1 MAZE-STATE-WIDTH))
+                                (< x (sub1 (MAZE-STATE-WIDTH)))
                                 (> y 0)
-                                (< y (sub1 MAZE-STATE-HEIGHT)))]
+                                (< y (sub1 (MAZE-STATE-HEIGHT))))]
                   #:when valid?)
          action))
+     
      (define (terminal? state)
-       (and (= (maze-state-x state) (sub1 MAZE-STATE-WIDTH))
-            (= (maze-state-y state) (sub1 MAZE-STATE-HEIGHT))))
+       (and (= (maze-state-x state) (sub1 (MAZE-STATE-WIDTH)))
+            (= (maze-state-y state) (sub1 (MAZE-STATE-HEIGHT)))))
+     
      ;;imperfect policy
      (define (policy-score state action)
        (case action
          [(right down) (policy-prob)]
          [else (- 1 (policy-prob))]))
+     
      (define (process-reward state action)
-       (if (terminal? (take-action state action)) 1.0 (local-reward)))
+       (if (terminal? (take-action state action))
+           1.0
+           (local-reward)))
+     
      (define (take-action state action)
        (define x (maze-state-x state))
        (define y (maze-state-y state))
+       (define player (maze-state-player state))
        (case action
-         ['left (maze-state (sub1 x) y)]
-         ['right (maze-state (add1 x) y)]
-         ['up (maze-state x (sub1 y))]
-         ['down (maze-state x (add1 y))]
+         ['left (maze-state (sub1 x) y player)]
+         ['right (maze-state (add1 x) y player)]
+         ['up (maze-state x (sub1 y) player)]
+         ['down (maze-state x (add1 y) player)]
          [else (error action "error")]))])
 
-  (define (init-state)
-    (maze-state 0 0)))
+  (define (init-state width height #:player [player 0])
+    (MAZE-STATE-WIDTH width)
+    (MAZE-STATE-HEIGHT height)
+    (maze-state 0 0 player)))
 
 (module mcts racket
   (provide mcts-search)
@@ -151,7 +165,11 @@
           (define edge (car edge-node))
           (set-mcts-node-n! node (add1 (mcts-node-n node)))
           (set-mcts-node-w! node (+ reward (mcts-node-w node)))
-          (loop (cdr reverse-path) (+ (if edge (mcts-edge-reward edge) 0) (* gamma reward))))))
+          (loop (cdr reverse-path)
+                (+ (if edge
+                       (mcts-edge-reward edge)
+                       0)
+                   (* gamma reward))))))
 
     (for ([i simulate-num])
       (define path-to-simulate (find-path-to-simulate #f root))
@@ -180,7 +198,9 @@
            (define actions (possible-action state))
            (define normalize-scores (normalize (map (curry policy-score state) actions)))
            (define action (weighted-sample actions normalize-scores))
-           (+ (* gamma (simulate (take-action state action))) (process-reward state action))]))
+           (define new-state (take-action state action))
+           (define same-player? (equal? (player state) (player new-state)))
+           (+ (* gamma (simulate new-state) (if same-player? 1 -1)) (process-reward state action))]))
 
       (define reward
         (mean (for/list ([i per-simulate-num])
@@ -194,26 +214,22 @@
 (require (submod "." mcts))
 (require (submod "." maze))
 (require (submod "." mcts-generics))
-(define episode-num 10)
 
-(define param-range (map (lambda (a) a) (inclusive-range 1 5 1)))
-param-range
-
-(for/list ([param 1])
-  (policy-prob 1.0)
-  (local-reward -0.5)
-  (/ (for/sum ([i episode-num])
-              (random-seed i)
-              (define state (init-state))
-              (let loop ([state state]
-                         [step 0])
-                (cond
-                  [(or (terminal? state) (>= step 100)) step]
-                  [else
-                   (define action
-                     (mcts-search state #:simulate-num 10 #:per-simulate-num 1 #:gamma 0.9))
-                   #;(displayln state)
-                   #;(displayln action)
-                   (loop (take-action state action) (add1 step))])))
-     episode-num
-     1.0))
+(define episode-num 100)
+(policy-prob 0.5)
+(local-reward -0.5)
+(/ (for/sum ([i episode-num])
+            (random-seed i)
+            (define state (init-state 3 3))
+            (let loop ([state state]
+                       [step 0])
+              (cond
+                [(or (terminal? state) (>= step 100)) step]
+                [else
+                 (define action
+                   (mcts-search state #:simulate-num 10 #:per-simulate-num 1 #:gamma 0.9))
+                 #;(displayln state)
+                 #;(displayln action)
+                 (loop (take-action state action) (add1 step))])))
+   episode-num
+   1.0)
